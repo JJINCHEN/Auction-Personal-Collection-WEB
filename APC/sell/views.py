023ea0e6e283ea1203db32785722dc671756a810
JJINCHEN <91751667+JJINCHEN@users.noreply.github.com>
2022-05-06 01:43:44 +0100

    Add files via upload
    Upload little

--- APC/sell/views.py
+++ APC/sell/views.py
@@ -326,6 +326,7 @@ def menu(request):
 @login_required
 def sell_userinfo(request):
     user = request.user
+    users = UserProfile.objects.filter()
     categories = Category.objects.filter()
     products = Product.objects.filter(sell_user=user)
     try:
@@ -478,7 +479,7 @@ def products(request):
                 all_products = all_products.filter(category=category_obj)
         # Display by search
         if search:
-            all_products = all_products.filter(name__contains=search)
+            all_products = all_products.filter(name__contains=search, detail=search)
         # Search all product names with search data
         for product in all_products:
             print(product.name)
